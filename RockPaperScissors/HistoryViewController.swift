8b46256a3e975209021762b7041337d885ae6ee7
ANDRES KWAN <andreskwan@gmail.com>
2015-11-04 06:17:43 -0500

    added - implementation of the methods required by the UITableViewDataSource Delegate

--- RockPaperScissors/HistoryViewController.swift
+++ RockPaperScissors/HistoryViewController.swift
@@ -10,11 +10,26 @@ import Foundation
 import UIKit
 
 class HistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var history: [RPSMatch]?
+    var history: [RPSMatch]!
     let cellIdentifier = "matchResultCellIdentifier"
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //what should happen if user hit the history button and there is not game played yet?
+        return history!.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //1 obtain(dequeue) the cell from the tableView with indexPath
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        //2 prepare data to be used in the cell
+        let match = history[indexPath.row]
+        //3 add data to the cell 
+        cell.textLabel?.text = match.resultString()
         
-        return 0
+        // If the cell has a detail label, we will put the evil scheme in.
+        if let detailTextLabel = cell.detailTextLabel {
+            detailTextLabel.text = match.messageResult()
+        }
+        return cell
     }
 }
